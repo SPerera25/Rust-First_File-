73586207082cb77857751d2185aa8bf8c983414e
S Perera <sperera1922@gmail.com>
2025-12-30 00:17:59 +0530

    First Rust File

---
+++ HelloWorld/target/debug/deps/HelloWorld-1bba9a347abdb43c.d
@@ -0,0 +1,5 @@
+C:\Users\PCSM\VS Code\Rust\Rust-First_File-\HelloWorld\target\debug\deps\HelloWorld-1bba9a347abdb43c.d: src\main.rs
+
+C:\Users\PCSM\VS Code\Rust\Rust-First_File-\HelloWorld\target\debug\deps\libHelloWorld-1bba9a347abdb43c.rmeta: src\main.rs
+
+src\main.rs:
---
+++ HelloWorld/target/debug/deps/HelloWorld.d
@@ -0,0 +1,5 @@
+C:\Users\PCSM\VS Code\Rust\Rust-First_File-\HelloWorld\target\debug\deps\HelloWorld.d: src\main.rs
+
+C:\Users\PCSM\VS Code\Rust\Rust-First_File-\HelloWorld\target\debug\deps\HelloWorld.exe: src\main.rs
+
+src\main.rs:
